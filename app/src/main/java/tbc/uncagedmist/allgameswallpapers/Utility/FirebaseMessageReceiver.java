41d6ce1b9ee014a5b5fd09d5284e0a070ec94503
Kundan Kumar <Kundan_kk52@outlook.com>
2021-06-12 13:32:28 +0530

    bug fixes on notification.

--- app/src/main/java/tbc/uncagedmist/allgameswallpapers/Utility/FirebaseMessageReceiver.java
+++ app/src/main/java/tbc/uncagedmist/allgameswallpapers/Utility/FirebaseMessageReceiver.java
@@ -2,74 +2,88 @@ package tbc.uncagedmist.allgameswallpapers.Utility;
 
 import android.annotation.SuppressLint;
 import android.app.Notification;
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import tbc.uncagedmist.allgameswallpapers.HomeActivity;
+import tbc.uncagedmist.allgameswallpapers.MainActivity;
 import tbc.uncagedmist.allgameswallpapers.R;
 
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
 public class FirebaseMessageReceiver extends FirebaseMessagingService {
 
-    public static final String CHANNEL_ID = "Game_Wallpaper";
-
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
 
+        String title = remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            sendNotificationAPI26(remoteMessage);
+            sendNotificationAPI26(title, body);
         }
         else    {
-            sendNotification(remoteMessage);
+            sendNotification(title, body);
         }
         super.onMessageReceived(remoteMessage);
     }
 
-    private void sendNotification(RemoteMessage remoteMessage) {
-        String title = remoteMessage.getNotification().getTitle();
-        String body = remoteMessage.getNotification().getBody();
+    private void sendNotificationAPI26(String title, String body) {
+        NotificationHelper helper;
+        Notification.Builder builder;
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(
+                this,
+                0,
+                notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        helper = new NotificationHelper(this);
+        builder = helper.getNotification(
+                title,
+                body,
+                defaultSoundUri,
+                contentIntent,
+                true);
+
+        helper.getManager().notify(1,builder.build());
+    }
+
+    private void sendNotification(String title, String body) {
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(
+                this,
+                0,
+                notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_notif)
                 .setContentTitle(title)
                 .setContentText(body)
                 .setAutoCancel(true)
-                .setPriority(Notification.PRIORITY_HIGH);
+                .setPriority(Notification.PRIORITY_HIGH)
+                .setContentIntent(contentIntent)
+                .setSound(defaultSoundUri);
 
         NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         manager.notify(1,builder.build());
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void sendNotificationAPI26(RemoteMessage remoteMessage) {
-
-        String title = remoteMessage.getNotification().getTitle();
-        String body = remoteMessage.getNotification().getBody();
-
-        NotificationChannel channel = new NotificationChannel(
-                CHANNEL_ID,
-                "Game_Wallpaper",
-                NotificationManager.IMPORTANCE_HIGH
-        );
-        getSystemService(NotificationManager.class).createNotificationChannel(channel);
-
-        Notification.Builder notification = new Notification.Builder(
-                this,CHANNEL_ID)
-                .setContentTitle(title)
-                .setContentText(body)
-                .setSmallIcon(R.drawable.ic_notif)
-                .setAutoCancel(true);
-
-        NotificationManagerCompat.from(this).notify(1,notification.build());
-    }
 }
